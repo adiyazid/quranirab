a9385462453108eef84d554835b4bcc8d831b0dc
tomyamGTX <hakimimdnoor90@gmail.com>
2021-12-23 13:23:14 +0800

    add appbar and dark mode

--- lib/views/data.from.firestore.dart
+++ lib/views/data.from.firestore.dart
@@ -1,5 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quranirab/themes/theme_model.dart';
+
+import 'nav.draw.dart';
 
 class DataFromFirestore extends StatefulWidget {
   const DataFromFirestore({Key? key}) : super(key: key);
@@ -37,31 +41,56 @@ class _DataFromFirestoreState extends State<DataFromFirestore> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(28.0),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Directionality(
-              textDirection: TextDirection.rtl,
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: _list
-                      .map((data) => TextSpan(
-                            text: data["text"],
-                            style: const TextStyle(
-                                fontSize: 40,
-                                fontFamily: "MeQuran2",
-                                color: Colors.black),
-                          ))
-                      .toList(),
+    return Consumer<ThemeModel>(
+        builder: (context, ThemeModel themeNotifier, child) {
+      return Scaffold(
+        drawer: navDrawer(),
+        appBar: AppBar(
+          backgroundColor: Colors.orange[700],
+          elevation: 0,
+          actions: [
+            IconButton(
+                icon: Icon(
+                    themeNotifier.isDark
+                        ? Icons.nightlight_round
+                        : Icons.wb_sunny,
+                    color: themeNotifier.isDark
+                        ? Colors.white
+                        : Colors.grey.shade900),
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                })
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: _list
+                        .map((data) => TextSpan(
+                              text: data["text"],
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  fontFamily: "MeQuran2",
+                                  color: (themeNotifier.isDark)
+                                      ? Colors.white
+                                      : Colors.black),
+                            ))
+                        .toList(),
+                  ),
                 ),
               ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
