a12b07a9b13a39fda33e90179a7f6888315c574b
tomyamGTX <hakimimdnoor90@gmail.com>
2022-01-11 10:52:28 +0800

    latest

--- lib/facebook/screens/Appbar/appbar.dart
+++ lib/facebook/screens/Appbar/appbar.dart
@@ -8,7 +8,6 @@ class Appbar extends StatefulWidget {
   @override
   _AppbarState createState() => _AppbarState();
 }
-
 class _AppbarState extends State<Appbar> {
   @override
   Widget build(BuildContext context) {
