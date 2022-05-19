23fc9d01b326c185d8dd5592ae2ea399e13ea111
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-05-19 17:17:06 +0800

    latest

--- lib/views/user.profile/user.profile.dart
+++ lib/views/user.profile/user.profile.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:quranirab/provider/user.provider.dart';
 import 'package:quranirab/widget/menu.dart';
