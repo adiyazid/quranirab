38907d2dedf8d7615bc0c735aad756362f59458f
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-06-16 11:25:27 +0800

    latest

--- lib/facebook/widgets/rooms.dart
+++ lib/facebook/widgets/rooms.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:quranirab/facebook/models/models.dart';
 import 'package:quranirab/facebook/widgets/profile_avatar.dart';
 import 'package:quranirab/facebook/widgets/responsive.dart';
-import '../palette.dart';
 
 class Rooms extends StatelessWidget {
   final List<User> onlineUsers;
@@ -56,37 +55,38 @@ class Rooms extends StatelessWidget {
 class _CreateRoomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return OutlineButton(
-      onPressed: () => print('Create Room'),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0),
-      ),
-      color: Colors.white,
-      borderSide: BorderSide(
-        width: 3.0,
-        color: Colors.blueAccent[100]!,
-      ),
-      textColor: Palette.grey,
-      child: Row(
-        children: [
-          // ShaderMask(
-          //   shaderCallback: (rect) =>
-          //       Palette.createRoomGradient.createShader(rect),
-          //   child: Icon(
-          //     Icons.video_call,
-          //     size: 35.0,
-          //     color: Colors.white,
-          //   ),
-          // ),
-          Icon(
-            Icons.video_call,
-            size: 35.0,
-            color: Colors.purple,
-          ),
-          const SizedBox(width: 4.0),
-          Text('Create\nRoom'),
-        ],
-      ),
-    );
+    return Container();
+    // return OutlineButton(
+    //   onPressed: () => print('Create Room'),
+    //   shape: RoundedRectangleBorder(
+    //     borderRadius: BorderRadius.circular(30.0),
+    //   ),
+    //   color: Colors.white,
+    //   borderSide: BorderSide(
+    //     width: 3.0,
+    //     color: Colors.blueAccent[100]!,
+    //   ),
+    //   textColor: Palette.grey,
+    //   child: Row(
+    //     children: [
+    //       // ShaderMask(
+    //       //   shaderCallback: (rect) =>
+    //       //       Palette.createRoomGradient.createShader(rect),
+    //       //   child: Icon(
+    //       //     Icons.video_call,
+    //       //     size: 35.0,
+    //       //     color: Colors.white,
+    //       //   ),
+    //       // ),
+    //       Icon(
+    //         Icons.video_call,
+    //         size: 35.0,
+    //         color: Colors.purple,
+    //       ),
+    //       const SizedBox(width: 4.0),
+    //       Text('Create\nRoom'),
+    //     ],
+    //   ),
+    // );
   }
 }
