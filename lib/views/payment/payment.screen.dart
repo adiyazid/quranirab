f418fce0e45365bce4bacdb4faa87616b7de6ecb
amiruhaf <wan.amiru@yahoo.com>
2022-06-27 18:24:31 +0800

    payment

--- lib/views/payment/payment.screen.dart
+++ lib/views/payment/payment.screen.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart' hide Card;
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:quranirab/models/payment.output.dart';
@@ -20,6 +23,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
   final _phone = TextEditingController();
   var custId = GetStorage().read('custID');
   PaymentOutput? _paymentOutput;
+
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   Future<void> checkout(BuildContext context, String phone) async {
     try {
@@ -106,63 +116,159 @@ class _PaymentScreenState extends State<PaymentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              if (custId == null)
-                TextField(
-                  controller: _phone,
-                  decoration: InputDecoration(label: Text('Phone Number')),
-                ),
-              if (custId == null)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      if (_phone.text.isNotEmpty) {
-                        await checkout(context, _phone.text);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Insert Phone Number')));
-                      }
-                    },
-                    child: const Text('Check Out'),
-                  ),
-                ),
-              if (_paymentOutput != null)
-                Flexible(
-                  child: ListView(
+      backgroundColor: Colors.teal[50],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Flutter Credit Card View'),
+      ),
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Column(
+          children: [
+            CreditCardWidget(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode,
+              showBackView: isCvvFocused,
+              obscureCardNumber: true,
+              obscureCardCvv: true, onCreditCardWidgetChange: (CreditCardBrand ) {  },),
+            Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      ListTile(
-                          title: Text('Payment ID'),
-                          subtitle: Text(_paymentOutput!.id)),
-                      ListTile(
-                          title: Text('Payment Status'),
-                          subtitle: Text(_paymentOutput!.status)),
-                      ListTile(
-                          title: Text('Payment Amount'),
-                          subtitle: Text(_paymentOutput!.currency +
-                              ' ' +
-                              _paymentOutput!.amount.toString().substring(
-                                  0,
-                                  _paymentOutput!.amount.toString().length -
-                                      2))),
+                      CreditCardForm(cardNumber: cardNumber,
+                        expiryDate: expiryDate,
+                        cardHolderName: cardHolderName,
+                        cvvCode: cvvCode,
+                        onCreditCardModelChange: onCreditCardModelChange,
+                        themeColor: Colors.blue,
+                        formKey: formKey,
+                        cardNumberDecoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Number',
+                            hintText: 'xxxx xxxx xxxx xxxx'
+                        ),
+                        expiryDateDecoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Expired Date',
+                            hintText: 'xx/xx'
+                        ),
+                        cvvCodeDecoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'CVV',
+                            hintText: 'xxx'
+                        ),
+                        cardHolderDecoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Card Holder',
+                        ),
+                      ),
                       ElevatedButton(
-                          onPressed: () {
-                            launchUrl(Uri.parse(
-                                _paymentOutput!.charges.data.last.receiptUrl));
-                          },
-                          child: Text('Get Receipt')),
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            primary: Color(0xff1b447b)
+
+                        ),
+                        child: Container(
+                          margin: EdgeInsets.all(8.0),
+                          child: Text(
+                            'validate',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'halter',
+                              fontSize: 14,
+                              package: 'flutter_credit_card',
+                            ),
+                          ),
+                        ),
+                        onPressed: (){
+                          if(formKey.currentState!.validate()){
+                            print('valid');
+                          }
+                          else{
+                            print('inValid');
+                          }
+                        },)
                     ],
                   ),
-                )
-            ],
-          ),
+                )),
+          ],
         ),
       ),
     );
+  }
+  // {
+  //   return Scaffold(
+  //     appBar: AppBar(),
+  //     body: Center(
+  //       child: Padding(
+  //         padding: const EdgeInsets.all(16.0),
+  //         child: Column(
+  //           children: [
+  //             if (custId == null)
+  //               TextField(
+  //                 controller: _phone,
+  //                 decoration: InputDecoration(label: Text('Phone Number')),
+  //               ),
+  //             if (custId == null)
+  //               Padding(
+  //                 padding: const EdgeInsets.all(8.0),
+  //                 child: ElevatedButton(
+  //                   onPressed: () async {
+  //                     if (_phone.text.isNotEmpty) {
+  //                       await checkout(context, _phone.text);
+  //                     } else {
+  //                       ScaffoldMessenger.of(context).showSnackBar(
+  //                           SnackBar(content: Text('Insert Phone Number')));
+  //                     }
+  //                   },
+  //                   child: const Text('Check Out'),
+  //                 ),
+  //               ),
+  //             if (_paymentOutput != null)
+  //               Flexible(
+  //                 child: ListView(
+  //                   children: [
+  //                     ListTile(
+  //                         title: Text('Payment ID'),
+  //                         subtitle: Text(_paymentOutput!.id)),
+  //                     ListTile(
+  //                         title: Text('Payment Status'),
+  //                         subtitle: Text(_paymentOutput!.status)),
+  //                     ListTile(
+  //                         title: Text('Payment Amount'),
+  //                         subtitle: Text(_paymentOutput!.currency +
+  //                             ' ' +
+  //                             _paymentOutput!.amount.toString().substring(
+  //                                 0,
+  //                                 _paymentOutput!.amount.toString().length -
+  //                                     2))),
+  //                     ElevatedButton(
+  //                         onPressed: () {
+  //                           launchUrl(Uri.parse(
+  //                               _paymentOutput!.charges.data.last.receiptUrl));
+  //                         },
+  //                         child: Text('Get Receipt')),
+  //                   ],
+  //                 ),
+  //               )
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  void onCreditCardModelChange(CreditCardModel creditCardModel){
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
