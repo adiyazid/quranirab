d47ebd4bf6d33c215af9c1ea744344f12f035f16
Hakimi Mdnoor <74492300+tomyamGTX@users.noreply.github.com>
2022-07-04 13:45:26 +0800

    Delete .env.dart

--- lib/.env.dart
+++
@@ -1,6 +0,0 @@
-// Get the data from Stripe Dashboard
-
-const String StripePublishableKey =
-    'pk_live_51KUoMmH8qbOVfW8TZESij6zhvHSRYGu2hoYfYJQfFq5oMBboEtEAJ52YXUgQHHcdbJQu2CTvX2UX4lLWVuIRn7np00Kt8WIQiW';
-const String StripeSecretKey =
-    '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';
