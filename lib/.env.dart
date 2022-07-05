587e7b375590fbecd66974fc4a8d4f550d25b0c4
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-07-05 10:53:16 +0800

    fix translation

--- lib/.env.dart
+++ lib/.env.dart
@@ -1,6 +1,4 @@
 // Get the data from Stripe Dashboard
 
-const String StripePublishableKey =
-    'pk_live_51KUoMmH8qbOVfW8TZESij6zhvHSRYGu2hoYfYJQfFq5oMBboEtEAJ52YXUgQHHcdbJQu2CTvX2UX4lLWVuIRn7np00Kt8WIQiW';
-const String StripeSecretKey =
-    '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';
+const String StripePublishableKey = 'your stripe publishkey';
+const String StripeSecretKey = 'your stripe secret key';
