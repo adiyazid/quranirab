6b86d032971f9946408b00640d2c1529b0c2a5c1
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-08-10 19:28:37 +0800

    change key

--- lib/.env.dart
+++ lib/.env.dart
@@ -1,4 +1,4 @@
 const String StripePublishableKey =
-    'pk_live_51KUoMmH8qbOVfW8TZESij6zhvHSRYGu2hoYfYJQfFq5oMBboEtEAJ52YXUgQHHcdbJQu2CTvX2UX4lLWVuIRn7np00Kt8WIQiW';
+    '';
 const String StripeSecretKey =
-    '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';
+    '';
